6662cc6d85298de2d44440b06b4b88706e0e07a2
lh99j <grand2181@gmail.com>
2023-08-08 15:17:00 +0900

    스위프트 for문, \()

--- codingTest/codingTest/main.swift
+++ codingTest/codingTest/main.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+var number = Int(readLine()!)!
+
+//1. 구구단 출력
+for i in 1...9{
+    //방법 1
+    //print(number, "*", i, "=", number * i, separator: " ")
+
+    //방법 2
+    print("\(number) * \(i) = \(number * i)")
+}
+
+
+
+// number만큼 반복
+var number = Int(readLine()!)!
+
+for _ in 1...number{
+    let numberArray = readLine()!.split(separator: " ").map{ Int(String($0))! }
+    
+    print(numberArray[0] + numberArray[1])
+}
+
