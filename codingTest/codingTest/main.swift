cfc0abb160a30d16903f6777b25bc5f0af55e66b
lh99j <grand2181@gmail.com>
2023-08-10 12:22:15 +0900

    별찍기 1

--- codingTest/codingTest/main.swift
+++ codingTest/codingTest/main.swift
@@ -2,30 +2,18 @@
 //  main.swift
 //  codingTest
 //
-//  Created by 임형준 on 2023/08/07.
+//  Created by 임형준 on 2023/08/10.
 //
 
 import Foundation
 
-var number = Int(readLine()!)!
+//별찍기 1
+let cnt = Int(readLine()!)!
 
-//1. 구구단 출력
-for i in 1...9{
-    //방법 1
-    //print(number, "*", i, "=", number * i, separator: " ")
-
-    //방법 2
-    print("\(number) * \(i) = \(number * i)")
-}
-
-
-
-// number만큼 반복
-var number = Int(readLine()!)!
-
-for _ in 1...number{
-    let numberArray = readLine()!.split(separator: " ").map{ Int(String($0))! }
-    
-    print(numberArray[0] + numberArray[1])
+for i in 1...cnt{
+    for _ in 1...i{
+        print("*", terminator: "")
+    }
+    print()
 }
 
