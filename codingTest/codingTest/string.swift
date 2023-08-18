ed91ca7e5cbf9ef055dfcfe1bc2d576dfdc0eb3a
lh99j <grand2181@gmail.com>
2023-08-18 12:41:57 +0900

    백준 2908

--- codingTest/codingTest/string.swift
+++ codingTest/codingTest/string.swift
@@ -13,3 +13,19 @@ let str = Array(readLine()!).map{ Int(String($0))! }
 
 let total = str.reduce(0, +)
 print(total)
+
+//문제 2908
+var input = readLine()!.split(separator: " ")
+
+var firstArray = input[0].map{ Int(String($0))! }, secondArray = input[1].map{ Int(String($0))! }
+
+firstArray = firstArray.reversed()
+secondArray = secondArray.reversed()
+
+let firstNum = firstArray.reduce(0, { $0 * 10 + $1 })
+let secondNum = secondArray.reduce(0, { $0 * 10 + $1 })
+
+let answer = firstNum > secondNum ? firstNum : secondNum
+
+print(answer)
+
