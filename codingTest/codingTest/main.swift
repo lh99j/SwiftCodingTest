6bbed258cc8bcea1d7d2cb48d74cf8f56f1767c9
lh99j <grand2181@gmail.com>
2023-10-14 22:37:08 +0900

    no message

--- codingTest/codingTest/main.swift
+++ codingTest/codingTest/main.swift
@@ -2,43 +2,26 @@
 //  main.swift
 //  codingTest
 //
-//  Created by 임형준 on 2023/08/10.
+//  Created by 임형준 on 2023/08/14.
 //
 
 import Foundation
 
-//별찍기 1
-let cnt = Int(readLine()!)!
+var input = readLine()!.split(separator: " ")
 
-for i in 1...cnt{
-    for _ in 1...i{
-        print("*", terminator: "")
-    }
-    print()
-}
+var firstArray = input[0].map{ Int(String($0))! }, secondArray = input[1].map{ Int(String($0))! }
 
+firstArray = firstArray.reversed()
+secondArray = secondArray.reversed()
 
-//별찍기 2(반대)
-let cnt = Int(readLine()!)!
+let firstNum = firstArray.reduce(0, { $0 * 10 + $1 })
+let secondNum = secondArray.reduce(0, { $0 * 10 + $1 })
 
-for i in (1...cnt).reversed(){
-    for _ in 1...i{
-        print("*", terminator: "")
-    }
-    print()
-}
+let answer = firstNum > secondNum ? firstNum : secondNum
+
+print(answer)
 
 
-//별찍기 3
-let cnt = Int(readLine()!)!
 
-for i in (1...cnt).reversed(){
-    for _ in 0..<(i - 1){
-        print(" ", terminator: "")
-    }
-    for _ in i...cnt{
-        print("*", terminator: "")
-    }
-    print()
-}
+
 
